53566595a83920d3f39a46f92080331e4dada882
youranreus <youranreus@qq.com>
2022-01-18 11:36:24 +0800

    feat: 使用MUI组件替换navbar基础样式

--- client/src/mobile/components/NavBar.jsx
+++ client/src/mobile/components/NavBar.jsx
@@ -4,28 +4,47 @@
  * @since 1.0
  */
 
-import { Link } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 
 export default function NavBar() {
-    return (
-        <div>
-            <ul style={{
-                position: "fixed",
-                bottom: '0px'
-            }}>
-                <li>
-                    <Link to="/">聊天</Link>
-                </li>
-                <li>
-                    <Link to="/relation">人际</Link>
-                </li>
-                <li>
-                    <Link to="/social">社交</Link>
-                </li>
-                <li>
-                    <Link to="/setting">设置</Link>
-                </li>
-            </ul>
-        </div>
-    )
+	const [value, setValue] = useState("chat");
+
+    const navigate = useNavigate();
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+        navigate('/'+newValue);
+	};
+
+	return (
+		<BottomNavigation
+			sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+			value={value}
+			onChange={handleChange}
+		>
+			<BottomNavigationAction
+				label="chat"
+				value=""
+				icon={<span>聊天</span>}
+			/>
+			<BottomNavigationAction
+				label="relation"
+				value="relation"
+				icon={<span>人际</span>}
+			/>
+			<BottomNavigationAction
+				label="social"
+				value="social"
+				icon={<span>社交</span>}
+			/>
+			<BottomNavigationAction
+				label="setting"
+				value="setting"
+				icon={<span>设置</span>}
+			/>
+		</BottomNavigation>
+	);
 }
