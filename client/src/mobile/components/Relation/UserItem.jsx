2f27124020c8121a38206e7ad418e7dc8f51b2ba
季悠然 <youranreus@qq.com>
2022-02-14 11:32:07 +0800

    feat: 修改备注功能

--- client/src/mobile/components/Relation/UserItem.jsx
+++ client/src/mobile/components/Relation/UserItem.jsx
@@ -5,50 +5,115 @@ import Typography from "@mui/material/Typography";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import UserItemBtn from "./UserItemBtn";
 import "./style.css";
 
 export default function UserItem(props) {
-	const [expanded, setExpanded] = useState(false)
-	const navigate = useNavigate()
+	const [expanded, setExpanded] = useState(false);
+	const [remarks, setRemarks] = useState(props.remarks);
+	const [newRemark, setNewRemark] = useState(props.remarks);
+	const [open, setOpen] = useState(false);
 
-	const jump = function(dest) {
-		console.log('跳转至动态')
-		navigate(`/${dest}/${props.name}`)
-	}
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const navigate = useNavigate();
+
+	const jump = (dest) => {
+		console.log("跳转至" + dest);
+		navigate(`/${dest}/${props.name}`);
+	};
+
+	const handleNewRemark = () => {
+		setRemarks(newRemark);
+		handleClose();
+	};
+
+	const handleChange = (event) => {
+		setNewRemark(event.target.value);
+	};
 
 	return (
-		<Accordion
-			expanded={expanded}
-			onChange={()=>{setExpanded(!expanded)}}
-            className="user-item-wrapper"
-		>
-			<AccordionSummary
-				className="user-item-header"
+		<>
+			<Accordion
+				expanded={expanded}
+				onChange={() => {
+					setExpanded(!expanded);
+				}}
+				className="user-item-wrapper"
 			>
-				<div className="user-item">
-					<Avatar
-						alt={props.name}
-						src={props.avatar}
-						sx={{ width: "3rem", height: "3rem" }}
+				<AccordionSummary className="user-item-header">
+					<div className="user-item">
+						<Avatar
+							alt={props.name}
+							src={props.avatar}
+							sx={{ width: "3rem", height: "3rem" }}
+						/>
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{ lineHeight: "3rem" }}
+						>
+							<span style={{ opacity: 0.2, marginRight: "1rem" }}>
+								{remarks ? `(${remarks})` : ""}
+							</span>
+							{props.name}
+						</Typography>
+					</div>
+				</AccordionSummary>
+				<AccordionDetails>
+					<div className="user-item-btn-group">
+						<UserItemBtn
+							title="动态"
+							size={4}
+							action={() => jump("moment")}
+						/>
+						<UserItemBtn
+							title="聊天"
+							size={4}
+							action={() => jump("chat")}
+						/>
+						<UserItemBtn
+							title="备注"
+							size={4}
+							action={handleClickOpen}
+						/>
+						<UserItemBtn title="删除" size={4} type="warning" />
+					</div>
+				</AccordionDetails>
+			</Accordion>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>给{props.name}备注</DialogTitle>
+				<DialogContent>
+					<TextField
+						autoFocus
+						margin="dense"
+						id="remarks"
+						label="备注"
+						type="text"
+						fullWidth
+						variant="standard"
+						value={newRemark}
+						onChange={handleChange}
 					/>
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ lineHeight: "3rem" }}
-					>
-						{props.name}
-					</Typography>
-				</div>
-			</AccordionSummary>
-			<AccordionDetails>
-                <div className="user-item-btn-group">
-                    <UserItemBtn title="动态" size={4} action={()=>jump('moment')}/>
-                    <UserItemBtn title="聊天" size={4} action={()=>jump('chat')}/>
-                    <UserItemBtn title="备注" size={4} />
-                    <UserItemBtn title="删除" size={4} type="warning"/>
-                </div>
-			</AccordionDetails>
-		</Accordion>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>取消</Button>
+					<Button onClick={handleNewRemark}>就酱</Button>
+				</DialogActions>
+			</Dialog>
+		</>
 	);
 }
--- client/src/mobile/pages/Relation/index.jsx
+++ client/src/mobile/pages/Relation/index.jsx
@@ -9,7 +9,7 @@ import UserItem from '../../components/Relation/UserItem.jsx'
 export default function Relation() {
     return (
         <>
-            <UserItem avatar="https://mui.com/static/images/avatar/1.jpg" name="季悠然"></UserItem>
+            <UserItem avatar="https://mui.com/static/images/avatar/1.jpg" name="季悠然" remarks="小号"></UserItem>
             <UserItem avatar="https://mui.com/static/images/avatar/1.jpg" name="卢本伟"></UserItem>
         </>
     )
